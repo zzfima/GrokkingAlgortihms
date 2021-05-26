92b1585bd70544f67b90261471efe44c5e2a3480
zfima <zzfima@gmail.com>
2021-05-26 09:43:27 +0300

    Add project files.

---
+++ GrokkingAlgortihms/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿namespace GrokkingAlgortihms
+{
+    public static class BinarySearch
+    {
+        public static int Search(int[] arr, int v)
+        {
+            int low = 0;
+            int high = arr.Length - 1;
+
+            while (true)
+            {
+                int middle = (high - low) / 2 + low;
+                if (arr[middle] == v)
+                    return middle;
+                else if (arr[middle] < v)
+                {
+                    if (low == middle || high == middle)
+                        break;
+                    low = middle;
+                }
+                else
+                {
+                    if (low == middle || high == middle)
+                        break;
+                    high = middle;
+                }
+
+
+            }
+
+            return -1;
+        }
+    }
+}
