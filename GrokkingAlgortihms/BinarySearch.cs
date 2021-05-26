0208cb0ee19db791f693ead4688ec4a66e5ec28b
zfima <zzfima@gmail.com>
2021-05-26 10:14:43 +0300

    rfct

--- GrokkingAlgortihms/BinarySearch.cs
+++ GrokkingAlgortihms/BinarySearch.cs
@@ -1,9 +1,12 @@
-﻿namespace GrokkingAlgortihms
+﻿using System;
+
+namespace GrokkingAlgortihms
 {
     public static class BinarySearch
     {
         public static int Search(int[] arr, int v)
         {
+            int cnt = 0;
             int low = 0;
             int high = arr.Length - 1;
 
@@ -11,7 +14,10 @@
             {
                 int middle = (high - low) / 2 + low;
                 if (arr[middle] == v)
+                {
+                    Console.WriteLine("Number of cycles was " + cnt);
                     return middle + 1;
+                }
                 else if (arr[middle] < v)
                 {
                     low = middle + 1;
@@ -20,8 +26,10 @@
                 {
                     high = middle - 1;
                 }
+                cnt++;
             }
 
+            Console.WriteLine("Number of cycles was " + cnt);
             return -1;
         }
     }
