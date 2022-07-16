8cf48fcfab2580127dc4d4413fc2a4053474587d
zzfima <zzfima@gmail.com>
2022-07-16 12:17:11 +0300

    start

---
+++ BinarySearch/BinarySearch/BinarySearcher.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.Remoting.Messaging;
+
+namespace BinarySearch
+{
+    public static class BinarySearcher
+    {
+        public static int Search(int[] array, int valueToFind)
+        {
+            //1 find median
+            //2 check if median is value if yes return true and finish
+            //3 if value less than median, move tail to median - 1, if value more than median, move head to median + 1
+            //4 run until head and tail not same
+
+            var headIndex = 0;
+            var tailIndex = array.Length - 1;
+
+            while (headIndex != tailIndex)
+            {
+                var medianIndex = Median(headIndex, tailIndex);
+                if (array[medianIndex] == valueToFind)
+                    return medianIndex;
+
+                if (valueToFind < array[medianIndex])
+                {
+                    tailIndex = medianIndex - 1;
+                }
+                else
+                {
+                    headIndex = medianIndex + 1;
+                }
+
+                if (headIndex == tailIndex)
+                {
+                    if (array[headIndex] == valueToFind)
+                        return headIndex;
+
+                    return -1;
+                }
+            }
+
+            return -1;
+        }
+
+        private static int Median(int headIndex, int tailIndex)
+        {
+            return headIndex + (tailIndex - headIndex) / 2;
+        }
+    }
+}
---
+++ BinarySearch/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using BinarySearch;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] array = { 4, 5, 6, 8, 33, 55, 77, 88, 4343 };
+            Assert.AreEqual(BinarySearcher.Search(array, 55), 5);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] array = { 4, 5, 6, 8, 33, 55, 77, 88, 4343, 6565 };
+            Assert.AreEqual(BinarySearcher.Search(array, 55), 5);
+        }
+    }
+}
