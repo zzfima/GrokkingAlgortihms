dd12e7bb3f97a06f06bd0e1c6d6f9276dddb3003
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-07-01 14:26:18 +0300

    rec

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,36 @@
+﻿int sum = 0;
+Console.WriteLine(ArraySum(new List<int> { 1, 2, 3, 4 }));
+Console.WriteLine(ArraySum1(new List<int> { 1, 2, 3, 4 }));
+Console.WriteLine(ArraySum2(new List<int> { 1, 2, 3, 4 }, 4));
+Console.ReadLine();
+
+
+int ArraySum(List<int> arr)
+{
+	if (arr.Count == 0)
+		return sum;
+	else
+	{
+		sum += arr[0];
+		arr.RemoveAt(0);
+		return ArraySum(arr);
+	}
+}
+
+int ArraySum1(List<int> arr)
+{
+	if (arr.Count == 0)
+		return 0;
+
+	var tmp = arr[0];
+	arr.RemoveAt(0);
+	return tmp + ArraySum1(arr);
+}
+
+int ArraySum2(List<int> arr, int n)
+{
+	if (n <= 0)
+		return 0;
+
+	return arr[n - 1] + ArraySum2(arr, n - 1);
+}
