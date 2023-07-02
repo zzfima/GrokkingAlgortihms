08e69db8b0bc3f6f53d1ce167608fd5bb50f8f00
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-07-02 20:26:23 +0300

    add find max number

--- DivideAndConquer/DivideAndConquer/Program.cs
+++ DivideAndConquer/DivideAndConquer/Program.cs
@@ -7,6 +7,9 @@ Console.WriteLine(sumArray.Sum(new List<int> { 1, 1, 5, 3 }));
 var calcArrayLength = new CalcArrayLength();
 Console.WriteLine(calcArrayLength.Calc(new List<int> { 1, 1, 5, 3, 6, 7, 8, 98 }));
 
+var calcMaxNumber = new CalcMaxNumber();
+Console.WriteLine(calcMaxNumber.Calc(new List<int> { 1, 1, 5, 356, 6, 7, 867, 98 }));
+
 
 Console.ReadLine();
 
@@ -52,5 +55,22 @@ public class CalcArrayLength
 
 		array.RemoveAt(0);
 		return 1 + Calc(array);
+	}
+}
+
+public class CalcMaxNumber
+{
+	public int Calc(List<int> array, int num = int.MinValue)
+	{
+		if (array.Count == 0)
+			return num;
+
+		var a1 = array[0];
+		array.RemoveAt(0);
+		var a2 = Calc(array);
+
+		if (a1 > a2)
+			return a1;
+		return a2;
 	}
 }
