44808597e6d02da160926e4e1e32df4329e8173e
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-07-07 13:31:28 +0300

    rfct

--- HIndex/HIndex/Program.cs
+++ HIndex/HIndex/Program.cs
@@ -1,22 +1,25 @@
 ﻿using System.Linq;
 
 var hindexFinder = new HIndexFinder();
-var publications = new int[] { 3, 0, 6, 1, 5 };
+var publications = new int[] { 7, 13, 0, 6, 1, 5 };
 
 Console.WriteLine($"H index of [{String.Join(", ", publications)}] is {hindexFinder.FindHVer1(publications)}");
+Console.WriteLine($"H index of [{String.Join(", ", publications)}] is {hindexFinder.FindHVer2(publications)}");
+
 
 Console.ReadLine();
 
 public class HIndexFinder
 {
+	//Complexity O(n^n)
 	internal int FindHVer1(int[] publications)
 	{
-		int h = 0;
 		int cnt = 0;
 
 		//size of h
-		for (h = 1; h < publications.Length; h++, cnt = 0)
+		for (int h = 1; h < publications.Length; h++)
 		{
+			cnt = 0;
 			//check how much h we have;
 			for (int j = 0; j < publications.Length; j++)
 				if (publications[j] >= h)
@@ -27,6 +30,19 @@ public class HIndexFinder
 				}
 
 			if (cnt < h)
+				return h - 1;
+		}
+		return -1;
+	}
+
+	//Complexity O(n*log(n) + n)
+	internal int FindHVer2(int[] publications)
+	{
+		Array.Sort(publications);
+		var n = publications.Length;
+		for (int h = 1; h < n; h++)
+		{
+			if (publications[n - h] < h)
 				return h - 1;
 		}
 		return -1;
